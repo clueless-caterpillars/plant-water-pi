fcc27780df531462dfff5924242a38b50e545079
Nate Brown <nathan.d.brown@gmail.com>
2023-06-07 21:50:00 -0700

    cognito user pool, adding signup with amplify to frontend

---
+++ src/aws/aws-exports.js
@@ -0,0 +1,10 @@
+const awsmobile = {
+  aws_project_region: 'us-west-2',
+  aws_cognito_identity_pool_id: 'us-west-2:5ae0523b-26ad-4ee1-b08a-2de7bd2cd97b',
+  aws_cognito_region: 'us-west-2',
+  aws_user_pools_id: 'us-west-2_iexBVW8r0',
+  aws_user_pools_web_client_id: '4r8dtn8t1irf8b4i5gnf64pt8c',
+  oauth: {},
+};
+
+export default awsmobile;
